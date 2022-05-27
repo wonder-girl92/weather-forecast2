da723938d5024479c8a3cfca2a1a959e16663d05
wonder-girl92 <81470730+wonder-girl92@users.noreply.github.com>
2022-05-27 04:00:15 +0300

    sorted cards

--- src/CardList/index.js
+++ src/CardList/index.js
@@ -7,25 +7,38 @@ export class CardList extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      orderBy: 'name',
+      orderBy: 'asc',
     };
+    this.handleOnChange = this.handleOnChange.bind(this);
   }
+
+ handleOnChange (event) {
+    this.setState({
+      orderBy: event.target.value,
+    });
+  }
+
   render () {
     const { orderBy } = this.state;
     const { citiesList } = this.props;
+    let sortedCitiesList = citiesList.sort();
+    if (orderBy === 'desc') {
+      sortedCitiesList.reverse();
+    }
+
     return (
       <>
-        <select className="Select" value={orderBy}>
-          <option value="name"> By name </option>
-          <option value="temp"> By temperature </option>
-        </select>
+        <select
+          className="Select"
+          value={orderBy}
+        onChange={this.handleOnChange}>
+          <option value="asc"> By name asc </option>
+          <option value="desc"> By name desc </option>
+          </select>
 
         <div className="CardList">
         {
-          citiesList.map(city => {
-            return (
-              <Card key={city} city={city} />
-)})
+          sortedCitiesList.map(city => <Card key={city} city={city} />)
         }
       </div>
       </>
