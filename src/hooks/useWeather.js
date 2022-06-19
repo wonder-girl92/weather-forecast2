63d47b451ebad2e86db03e0be7befdfbbe888d0e
wonder-girl92 <81470730+wonder-girl92@users.noreply.github.com>
2022-06-19 06:13:53 +0300

    eng

--- src/hooks/useWeather.js
+++ src/hooks/useWeather.js
@@ -4,7 +4,7 @@ import { API_KEY } from '../settings';
 export const useWeather = (city) => {
   const [data, setData] = useState(null);
   useEffect(() => {
-    fetch (`http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${city}&lang=ru`)
+    fetch (`http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${city}`)
       .then(res => res.json())
       .then(setData);
   }, [city]);
