13f714b94f042fe9c0f3ae0378ac8961c285c414
wonder-girl92 <81470730+wonder-girl92@users.noreply.github.com>
2022-06-20 12:34:10 +0300

    deploy

--- src/DailyCard/index.js
+++ src/DailyCard/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 export const DailyCard = ({ dailyCard }) => {
-  const {day, date, date_epoch} = dailyCard;
+  const {day, date_epoch} = dailyCard;
   const {condition, avgtemp_c} = day;
   const {text, icon} = condition;
   const currentDate = new Date(date_epoch * 1000);
@@ -9,7 +9,7 @@ export const DailyCard = ({ dailyCard }) => {
   return (
     <div className="DailyCard">
       <div> {currentDate.toString().split(' ')[0]} </div>
-      <img className="Icon" src={icon} />
+      <img className="Icon" alt="" src={icon} />
      <div> {avgtemp_c.toFixed()}° </div>
      <div> {text} </div>
     </div>
--- src/hooks/useForecast.js
+++ src/hooks/useForecast.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react';
 import { API_KEY } from '../settings'
 
 export const useForecast = (city) => {
